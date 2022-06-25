48dc412bf5cb0569a928b413eedfe2d7d4a9102a
nuclearzzet <niketjohn@outlook.com>
2022-06-25 20:24:43 +0530

    made some updates

--- src/app/models/user.go
+++ src/app/models/user.go
@@ -1,14 +1,11 @@
 package models
 
 type UserModel struct {
-	ID   int	
-	NAME string
-	AGE int
-}
-
-var Users = []UserModel{
-	{ID: 1, NAME: "Niket", AGE: 13},
-	{ID: 2, NAME: "Evan", AGE: 14},
-	{ID: 3, NAME: "Abhinav", AGE: 14},
-	{ID: 4, NAME: "Thomas", AGE: 14},
+	ID        uint
+	Username  string
+	Email     *string
+	FirstName string
+	LastName  string
+	CreatedAt int
+	LastLogin int
 }
